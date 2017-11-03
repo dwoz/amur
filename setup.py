cbebbf7cb2b7652872d29a8af6de571535f48578
Daniel Wozniak <daniel.wozniak@traxtech.com>
2017-11-03 14:32:23 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='amur',
+    version='0.1.0',
+    description='Migrations library',
+    author='Daniel Wozniak',
+    author_email='dan@woz.io',
+    url='https://github.com/dwoz/amur',
+    py_modules=['amur'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+)
+
